a394a18b0894734fafc8626e93d1403396a1563a
Scott Lawrence <bytbox@gmail.com>
2011-12-01 15:55:52 -0500

    Adding multiplication

--- builtins.go
+++ builtins.go
@@ -11,13 +11,63 @@ func builtinFunc(f func([]sexpr) sexpr) sexpr {
 
 var global = scope {
 	"nil": Nil,
-	"+": builtinFunc(builtinPlus),
-	"-": Nil,
-	"*": Nil,
-	"/": Nil,
+	"+": builtinFunc(builtinAdd),
+	"-": builtinFunc(builtinSub),
+	"*": builtinFunc(builtinMul),
+	"/": builtinFunc(builtinDiv),
 }
 
-func builtinPlus(ss []sexpr) sexpr {
+func builtinAdd(ss []sexpr) sexpr {
+	// add all numeric arguments
+	r := 0.
+	for _, s := range ss {
+		if s.kind != _ATOM {
+			panic("Invalid argument")
+		}
+		a := s.data.(atom)
+		if a.kind != _NUMBER {
+			panic("Invalid argument")
+		}
+		n := a.data.(float64)
+		r += n
+	}
+	return sexpr{_ATOM, atom{_NUMBER, r}}
+}
+
+func builtinSub(ss []sexpr) sexpr {
+	r := 0.
+	for _, s := range ss {
+		if s.kind != _ATOM {
+			panic("Invalid argument")
+		}
+		a := s.data.(atom)
+		if a.kind != _NUMBER {
+			panic("Invalid argument")
+		}
+		n := a.data.(float64)
+		r += n
+	}
+	return sexpr{_ATOM, atom{_NUMBER, r}}
+}
+
+func builtinMul(ss []sexpr) sexpr {
+	// add all numeric arguments
+	r := 1.
+	for _, s := range ss {
+		if s.kind != _ATOM {
+			panic("Invalid argument")
+		}
+		a := s.data.(atom)
+		if a.kind != _NUMBER {
+			panic("Invalid argument")
+		}
+		n := a.data.(float64)
+		r *= n
+	}
+	return sexpr{_ATOM, atom{_NUMBER, r}}
+}
+
+func builtinDiv(ss []sexpr) sexpr {
 	// add all numeric arguments
 	r := 0.
 	for _, s := range ss {
