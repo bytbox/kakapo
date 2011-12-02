e0fadd318840065284a94d1c41218acec1c961ad
Scott Lawrence <bytbox@gmail.com>
2011-12-01 20:12:42 -0500

    Removing more unnecessary code

--- builtins.go
+++ builtins.go
@@ -1,21 +1,12 @@
 package main
 
-// TODO
-func builtinSyntax() {
-
-}
-
-func builtinFunc(f func([]sexpr) sexpr) sexpr {
-	return f
-}
-
 var global = scope {
 	"nil": Nil,
 
 	// Arithmetic
-	"+": builtinFunc(builtinAdd),
-	"-": builtinFunc(builtinSub),
-	"*": builtinFunc(builtinMul),
-	"/": builtinFunc(builtinDiv),
-	"%": builtinFunc(builtinMod),
+	"+": builtinAdd,
+	"-": builtinSub,
+	"*": builtinMul,
+	"/": builtinDiv,
+	"%": builtinMod,
 }
