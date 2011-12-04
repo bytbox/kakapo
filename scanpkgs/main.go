ebaac6ba3f87e603fc78dbe8cbf13fcf6bcefa0b
Scott Lawrence <bytbox@gmail.com>
2011-12-03 23:21:37 -0500

    Succesfully listing packages (but doesn't build)

--- scanpkgs/main.go
+++ scanpkgs/main.go
@@ -2,8 +2,47 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"os"
+	"path"
+	"regexp"
+	"runtime"
+	"strings"
 )
 
 func main() {
-	fmt.Printf("package main\n")
+	fmt.Println("package main")
+
+	goroot := runtime.GOROOT()
+	pkgDir := path.Join(goroot, "pkg", runtime.GOOS + "_" + runtime.GOARCH)
+
+	pkgs := make(map[string][]string)
+	fmt.Fprint(os.Stderr, "Scanning for packages...")
+	readPackages("", pkgDir, pkgs)
+
+	fmt.Println("import (")
+	for name, _ := range pkgs {
+		iName := "i_" + strings.Replace(name, "/", "_", -1)
+		fmt.Printf("\t%s \"%s\"\n", iName, name)
+	}
+	fmt.Println(")")
+}
+
+var isPkg = regexp.MustCompile("\\.a$")
+
+func readPackages(p string, d string, pkgs map[string][]string) {
+	ents, err := ioutil.ReadDir(path.Join(d, p))
+	if err != nil {
+		return
+	}
+	for _, ent := range ents {
+		n := ent.Name()
+		if ent.IsDir() {
+			readPackages(path.Join(p, n), d, pkgs)
+			continue
+		}
+		if isPkg.MatchString(n) {
+			pkgs[path.Join(p, n[0:len(n)-2])] = make([]string, 0)
+		}
+	}
 }
