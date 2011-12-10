315a26634099a9ee4ce475f6a17a07f4571c1e6f
Scott Lawrence <bytbox@gmail.com>
2011-12-10 00:41:20 -0500

    Filling out definition of type scope

--- scope.go
+++ scope.go
@@ -5,5 +5,19 @@ type scope struct {
 	parent *scope
 }
 
-var global *scope
+func (s *scope) lookup(sym string) sexpr {
+	v, ok := s.data[sym]
+	if ok {
+		return v
+	}
+	if s.parent != nil {
+		return s.parent.lookup(sym)
+	}
+	panic("undefined")
+}
 
+func (s *scope) define(sym string, val sexpr) {
+	s.data[sym] = val
+}
+
+var global *scope
