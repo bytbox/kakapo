7f06b81bdd630bd2f5a2d400e97ab7151a1136c8
Scott Lawrence <bytbox@gmail.com>
2011-12-11 02:51:03 -0500

    Implementing (read) (at the cost of some bugs)

--- lisp/interpret.go
+++ lisp/interpret.go
@@ -1,12 +1,19 @@
 package lisp
 
 import (
+	"fmt"
 	"io"
 )
 
-func EvalFrom(r io.Reader) {
-	var sc = make(chan sexpr)
+func EvalFrom(ior io.Reader) {
+	// TODO parse and eval in separate goroutines
 
-	go doParse(GetRuneScanner(r), sc)
-	doEval(sc)
+	//go doParse(GetRuneScanner(r), sc)
+
+	r := GetRuneScanner(ior)
+	for {
+		e := parse(r)
+		v := eval(global, e)
+		fmt.Println(asString(v))
+	}
 }
