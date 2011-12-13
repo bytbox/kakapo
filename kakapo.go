f10c1db7495be6648ab4a17c1ac0ce20af74e514
Scott Lawrence <bytbox@gmail.com>
2011-12-12 21:42:48 -0500

    Adding version flag, -V

--- kakapo.go
+++ kakapo.go
@@ -2,13 +2,26 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"strings"
 
 	. "./lisp"
 )
 
+const VERSION = `0.2`
+
+var (
+	version = flag.Bool("V", false, "Display version information and exit")
+)
+
 func main() {
 	flag.Parse()
 
+	if *version {
+		fmt.Printf("Kakapo %s\n", VERSION)
+		return
+	}
+
+	// Start the read-eval-print loop
 	EvalFrom(strings.NewReader(repl))
 }
