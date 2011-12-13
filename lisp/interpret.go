e8bf59381d3e6c4b1eecc684b46da56c94548ffa
Scott Lawrence <bytbox@gmail.com>
2011-12-12 21:40:26 -0500

    Using buffered input

--- lisp/interpret.go
+++ lisp/interpret.go
@@ -1,15 +1,14 @@
 package lisp
 
 import (
+	"bufio"
 	"io"
 )
 
 func EvalFrom(ior io.Reader) {
 	// TODO parse and eval in separate goroutines
 
-	//go doParse(GetRuneScanner(r), sc)
-
-	r := GetRuneScanner(ior)
+	r := bufio.NewReader(ior)
 	e, err := parse(r)
 	for err == nil {
 		eval(global, e)
