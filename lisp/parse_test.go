2aca6bb77b5d8bd19981550501a6195f30b03d4b
Scott Lawrence <bytbox@gmail.com>
2011-12-12 21:35:54 -0500

    tests for parse()

--- lisp/parse_test.go
+++ lisp/parse_test.go
@@ -32,3 +32,48 @@ func TestReadToken(t *testing.T) {
 		}
 	}
 }
+
+type parseTest struct{
+	str string
+	res sexpr
+}
+
+var parseTests = []parseTest{
+	{"1\n", 1.0},
+	{"5.5\n", 5.5},
+	{"5e-9\n", 5e-9},
+	{"x\n", sym("x")},
+	{"5%x\n", sym("5%x")},
+	{"\"a\"", "a"},
+
+	{"()", Nil},
+	{"(())", cons{nil, nil}},
+	{"(1)", cons{1.0, nil}},
+	{"(1 (2 3) ())",
+		cons{1.0, cons{cons{2.0, cons{3.0, nil}}, cons{nil, nil}}}},
+}
+
+func eqS(a sexpr, b sexpr) bool {
+	ac, ok := a.(cons)
+	if ok {
+		bc, ok := b.(cons)
+		if !ok {
+			return false
+		}
+		return eqS(ac.car, bc.car) && eqS(ac.cdr, bc.cdr)
+	}
+	return a == b
+}
+
+func TestParse(t *testing.T) {
+	for _, test := range parseTests {
+		r := strings.NewReader(test.str)
+		res, err := parse(r)
+		if err != nil {
+			t.Errorf("ERROR: %s", err.Error())
+		}
+		if !eqS(res, test.res) {
+			t.Errorf("%s != %s", res, test.res)
+		}
+	}
+}
