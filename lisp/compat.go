52c64472ee2066b57409880d6bc309000d2fd7b1
Scott Lawrence <bytbox@gmail.com>
2011-12-17 09:24:31 -0500

    Various compat.go improvements

--- lisp/compat.go
+++ lisp/compat.go
@@ -111,6 +111,118 @@ func wrapGoval(r reflect.Value) sexpr {
 	return Nil
 }
 
+func forGo(v sexpr, typ reflect.Type) reflect.Value {
+	kind := typ.Kind()
+	switch kind {
+	case reflect.Bool:
+		return reflect.ValueOf(v != Nil)
+	case reflect.Int:
+		f, ok := v.(float64)
+		if !ok {
+			panic("Invalid argument")
+		}
+		return reflect.ValueOf(int(f))
+	case reflect.Int8:
+		f, ok := v.(float64)
+		if !ok {
+			panic("Invalid argument")
+		}
+		return reflect.ValueOf(int8(f))
+	case reflect.Int16:
+		f, ok := v.(float64)
+		if !ok {
+			panic("Invalid argument")
+		}
+		return reflect.ValueOf(int16(f))
+	case reflect.Int32:
+		f, ok := v.(float64)
+		if !ok {
+			panic("Invalid argument")
+		}
+		return reflect.ValueOf(int32(f))
+	case reflect.Int64:
+		f, ok := v.(float64)
+		if !ok {
+			panic("Invalid argument")
+		}
+		return reflect.ValueOf(int64(f))
+	case reflect.Uint:
+		f, ok := v.(float64)
+		if !ok {
+			panic("Invalid argument")
+		}
+		return reflect.ValueOf(uint(f))
+	case reflect.Uint8:
+		f, ok := v.(float64)
+		if !ok {
+			panic("Invalid argument")
+		}
+		return reflect.ValueOf(uint8(f))
+	case reflect.Uint16:
+		f, ok := v.(float64)
+		if !ok {
+			panic("Invalid argument")
+		}
+		return reflect.ValueOf(uint16(f))
+	case reflect.Uint32:
+		f, ok := v.(float64)
+		if !ok {
+			panic("Invalid argument")
+		}
+		return reflect.ValueOf(uint32(f))
+	case reflect.Uint64:
+		f, ok := v.(float64)
+		if !ok {
+			panic("Invalid argument")
+		}
+		return reflect.ValueOf(uint64(f))
+	case reflect.Uintptr:
+		panic("Invalid argument") // TODO
+	case reflect.Float32:
+		f, ok := v.(float64)
+		if !ok {
+			panic("Invalid argument")
+		}
+		return reflect.ValueOf(float32(f))
+	case reflect.Float64:
+		f, ok := v.(float64)
+		if !ok {
+			panic("Invalid argument")
+		}
+		return reflect.ValueOf(f)
+	case reflect.Complex64:
+		panic("Invalid argument") // TODO
+	case reflect.Complex128:
+		panic("Invalid argument") // TODO
+	case reflect.Array:
+		panic("Invalid argument") // TODO
+	case reflect.Chan:
+		panic("Invalid argument") // TODO
+	case reflect.Func:
+		panic("Cannot do callbacks yet, sorry") // XXX TODO
+	case reflect.Interface:
+		// TODO do some checks
+		reflect.ValueOf(v)
+	case reflect.Map:
+		panic("Invalid argument") // TODO
+	case reflect.Ptr:
+		panic("Invalid argument") // TODO
+	case reflect.Slice:
+		panic("Invalid argument") // TODO
+	case reflect.String:
+		s, ok := v.(string)
+		if !ok {
+			panic("Invalid argument")
+		}
+		return reflect.ValueOf(s)
+	case reflect.Struct:
+		panic("Invalid argument") // TODO
+	case reflect.UnsafePointer:
+		panic("Invalid argument") // can't handle this
+	}
+	return reflect.ValueOf(v)
+}
+
 func wrapFunc(f interface{}) function {
 	// TODO patch reflect so we can do type compatibility-checking
 	return func(sc *scope, ss []sexpr) sexpr {
@@ -124,8 +236,16 @@ func wrapFunc(f interface{}) function {
 
 		vs := make([]reflect.Value, len(ss))
 		for i, s := range ss {
+			// get argument type
+			var at reflect.Type
+			if t.IsVariadic() && i >= ni-1 {
+				st := t.In(ni-1)
+				at = st.Elem()
+			} else {
+				at = t.In(i)
+			}
 			// TODO convert any cons and function arguments
-			vs[i] = reflect.ValueOf(s)
+			vs[i] = forGo(s, at)
 		}
 		r := fun.Call(vs)
 		if len(r) == 0 {
